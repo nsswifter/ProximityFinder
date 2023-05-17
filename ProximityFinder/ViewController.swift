9805eb77ec12d36c1bf58141359e7d36354482df
Mehdi Karami <mehdi2003karami2003@users.noreply.github.com>
2023-05-18 00:10:19 +0330

    Initial Commit

---
+++ ProximityFinder/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  ProximityFinder
+//
+//  Created by Mehdi Karami on 5/18/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
