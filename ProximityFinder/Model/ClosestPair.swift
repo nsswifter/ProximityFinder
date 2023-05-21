0676e2db7b7dfc3a085e9d02ef6900903ab9302b
Mehdi Karami <mehdi2003karami2003@users.noreply.github.com>
2023-05-22 02:27:44 +0330

    ClosestPair struct Added.

---
+++ ProximityFinder/Model/ClosestPair.swift
@@ -0,0 +1,15 @@
+//
+//  ClosestPair.swift
+//  ProximityFinder
+//
+//  Created by Mehdi Karami on 5/22/23.
+//
+
+import CoreGraphics
+
+/// Represents a pair of points and the distance between them.
+struct ClosestPair {
+    let point1: CGPoint
+    let point2: CGPoint
+    let distance: Double
+}
