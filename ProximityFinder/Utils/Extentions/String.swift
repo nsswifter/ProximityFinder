6dc48454c05db37903d930c47e578983b3a671ca
Mehdi Karami <mehdi2003karami2003@users.noreply.github.com>
2023-05-26 15:31:55 +0330

    Make App Localizable.

---
+++ ProximityFinder/Utils/Extentions/String.swift
@@ -0,0 +1,14 @@
+//
+//  String.swift
+//  ProximityFinder
+//
+//  Created by Mehdi Karami on 5/24/23.
+//
+
+import Foundation
+
+extension String {
+    var localized: String {
+        return NSLocalizedString(self, comment: "")
+    }
+}
