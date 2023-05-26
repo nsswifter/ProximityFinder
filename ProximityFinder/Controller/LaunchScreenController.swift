1eee0352bf27119f1b058ff1c10a0d2d32fc7f42
Mehdi Karami <mehdi2003karami2003@users.noreply.github.com>
2023-05-26 15:32:28 +0330

    Some Files Removed.

---
+++ ProximityFinder/Controller/LaunchScreenController.swift
@@ -0,0 +1,20 @@
+//
+//  LaunchScreenController.swift
+//  ProximityFinder
+//
+//  Created by Mehdi Karami on 5/22/23.
+//
+
+import UIKit
+
+class LaunchScreenController: UIView {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
